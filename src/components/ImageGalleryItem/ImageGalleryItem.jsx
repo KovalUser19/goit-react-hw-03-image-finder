f6683aada386eff897e559ad1bf8eaec88711d43
KovalUser19 <134967853+KovalUser19@users.noreply.github.com>
2023-12-20 13:43:45 +0200

    add

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,9 +1,10 @@
 
+import { nanoid } from 'nanoid'
 import css from './ImageGalleryItem.module.css'
 
 export const ImageGalleryItem=(props)=>{
     return (
-      props.options.map(el => <li onClick={()=>props.onClickModal(el)} key={el.id} className={css.ImageGalleryItem}>
+      props.options.map(el => <li onClick={()=>props.onClickModal(el)} key={nanoid()} className={css.ImageGalleryItem}>
         <img className={css.ImageGalleryItem_image} src={el.webformatURL} alt={el.tags} />
       </li>)
     )
